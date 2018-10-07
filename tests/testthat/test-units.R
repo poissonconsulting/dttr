0fe08ec8ee68389ac8444089cd6a821d7c8c1071
Joe Thorley <joe@poissonconsulting.ca>
2018-10-07 15:37:45 -0700

    test

--- tests/testthat/test-units.R
+++ tests/testthat/test-units.R
@@ -7,55 +7,59 @@ test_that("get", {
   dates_na <- dates
   is.na(dates_na[2]) <- TRUE
 
-  expect_identical(dtt_seconds(dates[integer(0)]), integer(0))
+  expect_identical(dtt_second(dates[integer(0)]), integer(0))
   expect_identical(dtt_seconds(dates[1]), 0L)
   expect_identical(dtt_seconds(dates), c(0L, 0L, 0L))
-  expect_identical(dtt_seconds(dates_na), c(0L, 0L, 0L))
+  expect_identical(dtt_second(dates_na), c(0L, 0L, 0L))
  
-  expect_identical(dtt_seconds(date_times[integer(0)]), integer(0))
+  expect_identical(dtt_second(date_times[integer(0)]), integer(0))
   expect_identical(dtt_seconds(date_times[1]), 0L)
   expect_identical(dtt_seconds(date_times), c(0L, 59L, 59L))
-  expect_identical(dtt_seconds(date_times_na), c(0L, NA, 59L))
+  expect_identical(dtt_second(date_times_na), c(0L, NA, 59L))
   
-  expect_identical(dtt_minutes(dates[integer(0)]), integer(0))
+  expect_identical(dtt_minute(dates[integer(0)]), integer(0))
   expect_identical(dtt_minutes(dates[1]), 0L)
   expect_identical(dtt_minutes(dates), c(0L, 0L, 0L))
-  expect_identical(dtt_minutes(dates_na), c(0L, 0L, 0L))
+  expect_identical(dtt_minute(dates_na), c(0L, 0L, 0L))
  
-  expect_identical(dtt_minutes(date_times[integer(0)]), integer(0))
+  expect_identical(dtt_minute(date_times[integer(0)]), integer(0))
   expect_identical(dtt_minutes(date_times[1]), 0L)
   expect_identical(dtt_minutes(date_times), c(0L, 59L, 59L))
-  expect_identical(dtt_minutes(date_times_na), c(0L, NA, 59L))
+  expect_identical(dtt_minute(date_times_na), c(0L, NA, 59L))
   
-  expect_identical(dtt_hours(dates[integer(0)]), integer(0))
+  expect_identical(dtt_hour(dates[integer(0)]), integer(0))
   expect_identical(dtt_hours(dates[1]), 0L)
   expect_identical(dtt_hours(dates), c(0L, 0L, 0L))
-  expect_identical(dtt_hours(dates_na), c(0L, 0L, 0L))
+  expect_identical(dtt_hour(dates_na), c(0L, 0L, 0L))
  
-  expect_identical(dtt_hours(date_times[integer(0)]), integer(0))
+  expect_identical(dtt_hour(date_times[integer(0)]), integer(0))
   expect_identical(dtt_hours(date_times[1]), 0L)
   expect_identical(dtt_hours(date_times), c(0L, 23L, 23L))
-  expect_identical(dtt_hours(date_times_na), c(0L, NA, 23L))
+  expect_identical(dtt_hour(date_times_na), c(0L, NA, 23L))
   
-  expect_identical(dtt_days(dates[integer(0)]), integer(0))
+  expect_identical(dtt_day(dates[integer(0)]), integer(0))
   expect_identical(dtt_days(dates[1]), 1L)
   expect_identical(dtt_days(dates), c(1L, 31L, 29L))
-  expect_identical(dtt_days(dates_na), c(1L, NA, 29L))
+  expect_identical(dtt_day(dates_na), c(1L, NA, 29L))
  
-  expect_identical(dtt_days(date_times[integer(0)]), integer(0))
+  expect_identical(dtt_day(date_times[integer(0)]), integer(0))
   expect_identical(dtt_days(date_times[1]), 1L)
   expect_identical(dtt_days(date_times), c(1L, 31L, 30L))
-  expect_identical(dtt_days(date_times_na), c(1L, NA, 30L))
+  expect_identical(dtt_day(date_times_na), c(1L, NA, 30L))
   
-  expect_identical(dtt_months(dates[integer(0)]), integer(0))
+  expect_identical(dtt_month(dates[integer(0)]), integer(0))
   expect_identical(dtt_months(dates[1]), 1L)
   expect_identical(dtt_months(dates), c(1L, 12L, 4L))
-  expect_identical(dtt_months(dates_na), c(1L, NA, 4L))
+  expect_identical(dtt_month(dates_na), c(1L, NA, 4L))
  
   expect_identical(dtt_months(date_times[integer(0)]), integer(0))
   expect_identical(dtt_months(date_times[1]), 1L)
   expect_identical(dtt_months(date_times), c(1L, 12L, 6L))
-  expect_identical(dtt_months(date_times_na), c(1L, NA, 6L))
+  expect_identical(dtt_month(date_times_na), c(1L, NA, 6L))
+  expect_identical(dtt_months(date_times[integer(0)]), integer(0))
+  expect_identical(dtt_months(date_times[1]), 1L)
+  expect_identical(dtt_months(date_times), c(1L, 12L, 6L))
+  expect_identical(dtt_month(date_times_na), c(1L, NA, 6L))
 })
 
 test_that("set 1", {
