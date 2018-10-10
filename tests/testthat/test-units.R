64c48e70476e0920b96a7aa25910974345faca14
Joe Thorley <joe@poissonconsulting.ca>
2018-10-09 17:04:01 -0700

    debugging travis

--- tests/testthat/test-units.R
+++ tests/testthat/test-units.R
@@ -103,11 +103,18 @@ test_that("set 1", {
   expect_identical(dates_na, as.Date(c("0001-10-28", NA, "0001-10-28")))
   
   dtt_years(dates_na) <- c(1L, 2L, 3L)
+  expect_is(dates_na, "vector")
+  expect_is(dates_na, "list")
+  expect_is(dates_na, "integer")
+  expect_is(dates_na, "numeric")
+  expect_is(dates_na, "character")
+  expect_identical(length(dates_na), 3L)
+  
   expect_identical(dates_na, as.Date(c("0001-10-28", NA, "0003-10-28")))
-  dtt_months(dates_na) <- c(2L, 3L, 4L)
-  expect_identical(dates_na, as.Date(c("0001-02-28", NA, "0003-04-28")))
-  dtt_days(dates_na) <- c(5L, 6L, 7L)
-  expect_identical(dates_na, as.Date(c("0001-02-05", NA, "0003-04-07")))
+#  dtt_months(dates_na) <- c(2L, 3L, 4L)
+#  expect_identical(dates_na, as.Date(c("0001-02-28", NA, "0003-04-28")))
+#  dtt_days(dates_na) <- c(5L, 6L, 7L)
+#  expect_identical(dates_na, as.Date(c("0001-02-05", NA, "0003-04-07")))
 })
 
 test_that("units", {
