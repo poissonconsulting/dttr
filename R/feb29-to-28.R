e6805a7aff9c59124b40e938ebf3d3eb50ca59f9
Joe Thorley <joe@poissonconsulting.ca>
2018-10-12 19:28:54 -0700

    add feb29_to_28

---
+++ R/feb29-to-28.R
@@ -0,0 +1,15 @@
+#' Feb 29 to Feb 28
+#'
+#' @param x A Date or POSIXct vector.
+#'
+#' @return The modified Date or POSIXct vector.
+#' @export
+#'
+#' @examples
+#' dtt_feb29_to_28(as.Date("2004-02-29"))
+dtt_feb29_to_28 <- function(x) {
+  check_dtt(x)
+  wch <- which(dtt_month(x) == 2L && dtt_day(x) == 29L)
+  dtt_day(x[wch]) <- 28L
+  x
+}
