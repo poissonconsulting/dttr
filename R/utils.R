ce4daea8be09b1dc94a2544173ab8cdbfbf498d3
Joe Thorley <joe@poissonconsulting.ca>
2018-10-19 14:32:26 -0700

    adjust units

--- R/utils.R
+++ R/utils.R
@@ -55,3 +55,19 @@ dtt_units_per_unit <- function(units = "seconds", unit = "days") {
   check_scalar(unit, .units)
   as_numeric(dtt_duration(1, unit)) / as_numeric(dtt_duration(1, units))
 }
+
+#' Adjust Units
+#'
+#' @param x An integer or numeric vector
+#' @param from A string of the original units.
+#' @param to A string of the new units.
+#'
+#' @return A numeric vector.
+#' @export
+#'
+#' @examples
+#' dtt_adjust_units(60, to = "minutes")
+dtt_adjust_units <- function(x, from = "seconds", to = "seconds") {
+  checkor(check_vector(x, 1), check_vector(x, 1L))
+  x * dtt_units_per_unit(to, from)
+}
--- tests/testthat/test-utils.R
+++ tests/testthat/test-utils.R
@@ -5,3 +5,9 @@ test_that("units_per_unit", {
   expect_identical(dtt_units_per_unit("hours"), 24)
   expect_identical(dtt_units_per_unit("minutes", "seconds"), 1/60)
 })
+
+test_that("adjust_units", {
+  expect_identical(dtt_adjust_units(1), 1)
+  expect_identical(dtt_adjust_units(1, "minutes"), 60)
+  expect_identical(dtt_adjust_units(1, to = "minutes"), 1/60)
+})
