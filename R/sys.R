3a3aaa009dfaef417c9473173a92c1b7db94dbd8
Joe Thorley <joe@poissonconsulting.ca>
2019-04-29 09:38:19 -0700

    .Date and .POSIXct

--- R/sys.R
+++ R/sys.R
@@ -1,2 +1,13 @@
 .units <- c("seconds", "minutes", "hours", "days", "months", "years")
 .units_Date <- c("days", "months", "years")
+
+.Date <- function(x) {
+  class(x) <- "Date"
+  x
+}
+
+.POSIXct <- function(x, tz) {
+  class(x) <- c("POSIXct", "POSIXt")
+  attr(x, "tzone") <- tz
+  x
+}
