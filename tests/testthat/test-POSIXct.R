90ad5f1886cbbb1a9c5f8ca4ea5113bf57a2494f
Joe Thorley <joe@poissonconsulting.ca>
2019-05-04 14:31:33 -0700

    platform sensitive tests

--- tests/testthat/test-POSIXct.R
+++ tests/testthat/test-POSIXct.R
@@ -20,18 +20,22 @@ test_that("POSIXct", {
   expect_identical(format(as.POSIXct(1.99, origin = as.POSIXct("1970-01-01", tz = "GMT")), tz = "GMT"), 
                    "1970-01-01 00:00:01")
   
-  expect_identical(format(as.POSIXct(-0.1, origin = as.POSIXct("1970-01-01", tz = "GMT")), tz = "GMT"), 
+  skip_on_os("linux")
+  skip_on_os("solaris")
+  skip_on_os("windows")
+
+  expect_identical(format(as.POSIXct(-0.1, origin = as.POSIXct("1970-01-01", tz = "GMT")), tz = "GMT"),
                    "1970-01-01 00:00:00")
-  expect_identical(format(as.POSIXct(-0.99, origin = as.POSIXct("1970-01-01", tz = "GMT")), tz = "GMT"), 
+  expect_identical(format(as.POSIXct(-0.99, origin = as.POSIXct("1970-01-01", tz = "GMT")), tz = "GMT"),
                    "1970-01-01 00:00:00")
-  expect_identical(format(as.POSIXct(-1, origin = as.POSIXct("1970-01-01", tz = "GMT")), tz = "GMT"), 
+  expect_identical(format(as.POSIXct(-1, origin = as.POSIXct("1970-01-01", tz = "GMT")), tz = "GMT"),
                    "1969-12-31 23:59:59")
-  expect_identical(format(as.POSIXct(-1.99, origin = as.POSIXct("1970-01-01", tz = "GMT")), tz = "GMT"), 
+  expect_identical(format(as.POSIXct(-1.99, origin = as.POSIXct("1970-01-01", tz = "GMT")), tz = "GMT"),
                    "1969-12-31 23:59:59")
-  
-  expect_identical(format(as.POSIXct(-1.99, origin = as.POSIXct("1970-01-01", tz = "GMT")), tz = "GMT"), 
+
+  expect_identical(format(as.POSIXct(-1.99, origin = as.POSIXct("1970-01-01", tz = "GMT")), tz = "GMT"),
                    "1969-12-31 23:59:59")
-  expect_identical(format(as.POSIXct(-1.99 + 1, origin = as.POSIXct("1970-01-01", tz = "GMT")), tz = "GMT"), 
+  expect_identical(format(as.POSIXct(-1.99 + 1, origin = as.POSIXct("1970-01-01", tz = "GMT")), tz = "GMT"),
                    "1970-01-01 00:00:00")
   expect_identical(format(as.POSIXct(-1.99 + 2, origin = as.POSIXct("1970-01-01", tz = "GMT")), tz = "GMT"), 
                    "1970-01-01 00:00:00")
