8b5f7fa56c98d964c961bbee00c88076c88b276c
Joe Thorley <joe@poissonconsulting.ca>
2019-05-04 12:58:33 -0700

    feb 29th to 28th

--- R/feb29-to-28.R
+++ R/feb29-to-28.R
@@ -1,4 +1,6 @@
 #' Feb 29 to Feb 28
+#' 
+#' Converts Feb 29 to Feb 28th
 #'
 #' @param x A Date or POSIXct vector.
 #'
@@ -8,7 +10,9 @@
 #' @examples
 #' dtt_feb29_to_28(as.Date("2004-02-29"))
 dtt_feb29_to_28 <- function(x) {
-  check_dtt(x)
+  checkor(check_vector(x, c(Sys.Date(), NA)),
+          check_vector(x, c(Sys.time(), NA_POSIXct_)))
+  
   wch <- which(dtt_month(x) == 2L & dtt_day(x) == 29L)
   dtt_day(x[wch]) <- 28L
   x
